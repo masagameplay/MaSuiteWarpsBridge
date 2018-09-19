8166bf0f6f5f87dc523e36433d87ffa98da43868
Masa <masa@simasasolutions.com>
2018-09-19 15:47:48 +0300

    Added configs

---
+++ src/main/java/fi/matiaspaavilainen/masuitewarps/Config.java
@@ -0,0 +1,45 @@
+package fi.matiaspaavilainen.masuitewarps;
+
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import java.io.File;
+import java.io.IOException;
+
+public class Config {
+
+    private MaSuiteWarps plugin;
+    public Config(MaSuiteWarps p){
+        plugin = p;
+    }
+    private FileConfiguration messages;
+    private FileConfiguration syntaxes;
+
+    public FileConfiguration getMessages() {
+        return this.messages;
+    }
+    public FileConfiguration getSyntaxes() {
+        return this.syntaxes;
+    }
+    public void createConfigs() {
+        File messageFile = new File(plugin.getDataFolder(), "messages.yml");
+        File syntaxFile = new File(plugin.getDataFolder(), "syntax.yml");
+        if (!messageFile.exists()) {
+            messageFile.getParentFile().mkdirs();
+            plugin.saveResource("custom.yml", false);
+        }
+        if (!syntaxFile.exists()) {
+            syntaxFile.getParentFile().mkdirs();
+            plugin.saveResource("syntax.yml", false);
+        }
+        messages = new YamlConfiguration();
+        syntaxes = new YamlConfiguration();
+        try {
+            messages.load(messageFile);
+            syntaxes.load(syntaxFile);
+        } catch (IOException | InvalidConfigurationException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- src/main/java/fi/matiaspaavilainen/masuitewarps/MaSuiteWarps.java
+++ src/main/java/fi/matiaspaavilainen/masuitewarps/MaSuiteWarps.java
@@ -4,9 +4,10 @@ import fi.matiaspaavilainen.masuitewarps.commands.Delete;
 import fi.matiaspaavilainen.masuitewarps.commands.List;
 import fi.matiaspaavilainen.masuitewarps.commands.Set;
 import fi.matiaspaavilainen.masuitewarps.commands.Teleport;
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class MaSuiteWarps extends JavaPlugin {
+public class MaSuiteWarps extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
@@ -17,12 +18,14 @@ public class MaSuiteWarps extends JavaPlugin {
         saveDefaultConfig();
         this.getServer().getPluginManager().registerEvents(new Sign(this), this);
         registerCommands();
+        new Config(this).createConfigs();
     }
 
-    private void registerCommands(){
+    private void registerCommands() {
         getCommand("warp").setExecutor(new Teleport(this));
         getCommand("setwarp").setExecutor(new Set(this));
         getCommand("delwarp").setExecutor(new Delete(this));
         getCommand("warps").setExecutor(new List(this));
     }
+
 }
