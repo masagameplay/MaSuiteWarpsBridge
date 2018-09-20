f9537b1b0ee88b20f11f9c51a41cf61693ae2aa5
Masa <masa@simasasolutions.com>
2018-09-20 21:31:03 +0300

    Warmup

--- src/main/java/fi/matiaspaavilainen/masuitewarps/commands/Teleport.java
+++ src/main/java/fi/matiaspaavilainen/masuitewarps/commands/Teleport.java
@@ -2,8 +2,10 @@ package fi.matiaspaavilainen.masuitewarps.commands;
 
 import com.google.common.io.ByteArrayDataOutput;
 import com.google.common.io.ByteStreams;
+import fi.matiaspaavilainen.masuitewarps.Config;
+import fi.matiaspaavilainen.masuitewarps.Countdown;
 import fi.matiaspaavilainen.masuitewarps.MaSuiteWarps;
-import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -16,6 +18,7 @@ public class Teleport implements CommandExecutor {
     public Teleport(MaSuiteWarps p) {
         plugin = p;
     }
+    private Config config = new Config(plugin);
 
     @Override
     public boolean onCommand(CommandSender cs, Command cmd, String label, String[] args) {
@@ -24,11 +27,34 @@ public class Teleport implements CommandExecutor {
         }
         if (args.length == 1) {
             Player p = (Player) cs;
-            ByteArrayDataOutput out = ByteStreams.newDataOutput();
-            out.writeUTF("WarpCommand");
-            out.writeUTF(p.getName());
-            out.writeUTF(args[0]);
-            p.sendPluginMessage(plugin, "BungeeCord", out.toByteArray());
+            if(plugin.getConfig().getInt("warmup") > 0){
+                MaSuiteWarps.warmups.put(p.getUniqueId(), System.currentTimeMillis());
+                cs.sendMessage(ChatColor.translateAlternateColorCodes('&', config.getMessages().getString("teleportation-started").replace("%time%", String.valueOf(plugin.getConfig().getInt("warmup")))));
+                new Countdown(plugin.getConfig().getInt("warmup"), plugin) {
+                    @Override
+                    public void count(int current) {
+                        if(current == 0){
+                            if(MaSuiteWarps.warmups.containsKey(p.getUniqueId())){
+                                ByteArrayDataOutput out = ByteStreams.newDataOutput();
+                                out.writeUTF("WarpCommand");
+                                out.writeUTF(p.getName());
+                                out.writeUTF(args[0]);
+                                p.sendPluginMessage(plugin, "BungeeCord", out.toByteArray());
+                                MaSuiteWarps.warmups.remove(p.getUniqueId());
+                            }
+                        }
+                    }
+
+                }.start();
+            }else{
+                ByteArrayDataOutput out = ByteStreams.newDataOutput();
+                out.writeUTF("WarpCommand");
+                out.writeUTF(p.getName());
+                out.writeUTF(args[0]);
+                p.sendPluginMessage(plugin, "BungeeCord", out.toByteArray());
+            }
+
+
             return true;
         } else if (args.length == 2) {
             Player p = (Player) cs;
